ef0bc961fb79aa72893ca409c43b777e88775c33
m_ennz <m_ennz@163.com>
2020-05-07 11:35:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using System;
+using NetTopologySuite.Samples.Voronoi;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Samples.Voronoi
+{
+    [TestFixture]
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        private static DelaunayExample delaunay;
+
+        public void Start()
+        {
+            SamplesTest(true);
+        }
+
+        [Test]
+        public void SamplesTest()
+        {
+            SamplesTest(false);
+        }
+
+        public void SamplesTest(bool readLine)
+        {
+            try
+            {
+                delaunay = new DelaunayExample();
+                delaunay.TestDelunayTrianle();
+            }
+            finally
+            {
+                if (readLine) Console.ReadLine();
+            }
+        }
+    }
+}
